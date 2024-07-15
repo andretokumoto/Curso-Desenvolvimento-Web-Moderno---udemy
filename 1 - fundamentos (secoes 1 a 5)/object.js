9afdcb0a956644b3c1a7e4aa6907884d3e6a5742
andretokumoto <andretokumoto@gmail.com>
2024-07-15 12:57:56 -0300

    objeto

---
+++ 1 - fundamentos (secoes 1 a 5)/object.js
@@ -0,0 +1,22 @@
+//criação de objeto
+
+const prod1 = {}
+prod1.nome - 'celular'
+prod1.preco = 1000.00
+
+console.log(prod1)
+
+//definição do objeto na declaração
+
+const prod2 = {
+    nome: 'Laptop',
+    preco: 5000.00
+}
+
+console.log(prod2)
+
+//null 
+let valor
+console.log(valor)
+valor = null
+console.log(valor)
